681451d0844428ec31e8e0a52de2992a2f3787cd
Suisuikitai <mag.cupw@gmail.com>
2021-04-16 23:11:44 +0900

    metaタグ追加

--- pages/answers/[id]/index.tsx
+++ pages/answers/[id]/index.tsx
@@ -1,6 +1,7 @@
 import Layout from '../../../components/Layout'
 import { Answer } from '../../../models/Answer'
 import { Question } from '../../../models/Question'
+import Head from 'next/head'
 
 type Props = {
   answer: Answer
@@ -13,9 +14,27 @@ export async function getServerSideProps({ query }) {
   return { props: json }
 }
 
+function getDescription(answer: Answer) {
+  const body = answer.body.trim().replace(/[ \r\n]/g, '')
+  if (body.length < 140) {
+    return body
+  }
+  return body.substring(0, 140) + '...'
+}
+
 export default function AnswersShow(props: Props) {
+  const description = getDescription(props.answer)
+
   return (
     <Layout>
+      <Head>
+        <meta name='description' key='description' content={description} />
+        <meta
+          property='og:description'
+          key='ogDescription'
+          content={description}
+        />
+      </Head>
       <div className='row justify-content-center'>
         <div className='col-12 col-md-6'>
           <>
